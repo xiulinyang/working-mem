0b9f23a7c85cdba15fd947e6bfa8f9c1badb9794
xiulinyang <xiulin.yang.compling@gmail.com>
2026-07-31 14:49:46 -0400

    eval all benchmarks

--- eval_zorro_all.sh
+++ eval_zorro_all.sh
@@ -1,33 +1,69 @@
 #!/bin/bash
-python benchmark_eval.py xiulinyang/linear_only_chunk_0 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/linear_only_chunk_128 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/linear_only_chunk_96 --eval_dataset zorro
-#
-python benchmark_eval.py xiulinyang/dynamic_only_chunk_96 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/dynamic_only_chunk_128 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/dynamic_only_chunk_0 --eval_dataset zorro
-#
-python benchmark_eval.py xiulinyang/linear_dyck_chunk_96 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/linear_dyck_chunk_128 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/linear_dyck_chunk_0 --eval_dataset zorro
 
-python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset zorro
-python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset zorro
+python benchmark_eval.py xiulinyang/linear_only_chunk_0 --eval_dataset posh
+python benchmark_eval.py xiulinyang/linear_only_chunk_128 --eval_dataset posh
+python benchmark_eval.py xiulinyang/linear_only_chunk_96 --eval_dataset posh
+#
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_96 --eval_dataset posh
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_128 --eval_dataset posh
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_0 --eval_dataset posh
+#
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_96 --eval_dataset posh
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_128 --eval_dataset posh
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_0 --eval_dataset posh
+
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset posh
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset posh
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset posh
+
+
+python benchmark_eval.py xiulinyang/linear_only_chunk_0 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/linear_only_chunk_128 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/linear_only_chunk_96 --eval_dataset blimp
+#
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_96 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_128 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_0 --eval_dataset blimp
+#
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_96 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_128 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_0 --eval_dataset blimp
+
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset blimp
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset blimp
 
 #
-#python benchmark_eval.py xiulinyang/linear_only_chunk_0 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/linear_only_chunk_128 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/linear_only_chunk_96 --eval_dataset multiblimp
-##
-#python benchmark_eval.py xiulinyang/dynamic_only_chunk_96 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/dynamic_only_chunk_128 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/dynamic_only_chunk_0 --eval_dataset multiblimp
-##
-#python benchmark_eval.py xiulinyang/linear_dyck_chunk_96 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/linear_dyck_chunk_128 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/linear_dyck_chunk_0 --eval_dataset multiblimp
+python benchmark_eval.py xiulinyang/linear_only_chunk_0 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/linear_only_chunk_128 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/linear_only_chunk_96 --eval_dataset scamp_plausible
 #
-#python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset multiblimp
-#python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset multiblimp
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_96 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_128 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_0 --eval_dataset scamp_plausible
+#
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_96 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_128 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_0 --eval_dataset scamp_plausible
+
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset scamp_plausible
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset scamp_plausible
+
+
+#
+python benchmark_eval.py xiulinyang/linear_only_chunk_0 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/linear_only_chunk_128 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/linear_only_chunk_96 --eval_dataset scamp_implausible
+#
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_96 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_128 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/dynamic_only_chunk_0 --eval_dataset scamp_implausible
+#
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_96 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_128 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/linear_dyck_chunk_0 --eval_dataset scamp_implausible
+
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_96 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_128 --eval_dataset scamp_implausible
+python benchmark_eval.py xiulinyang/dynamic_dyck_chunk_0 --eval_dataset scamp_implausible
